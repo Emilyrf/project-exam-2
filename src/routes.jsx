979acb2d61f03549d80a22207197c1aec682719d
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-01-17 11:57:56 +0100

    Create routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,22 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Layout from './components/Layout';
 import Homepage from './pages/Home';
 import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
 import VenuePage from './pages/Venue';
 import NotFound from './pages/NotFound';
 
+const queryClient = new QueryClient();
+
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-       <Route path='/'>
-       <Route index element={<Homepage />} />
-        <Route path='venue/:id' element={<VenuePage />} />
-        <Route path='login' element={<LoginPage/>} />
-        <Route path='register' element={<RegisterPage />} />
-        <Route path='*' element={<NotFound />} />
-       </Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Homepage />} />
+            <Route path='venue/:id' element={<VenuePage />} />
+            <Route path='login' element={<LoginPage />} />
+            <Route path='register' element={<RegisterPage />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
