5b7c39459cf92acbd4393b0b3fdc4f31b8756e73
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-01-26 12:43:32 +0100

    Implements token logic

--- src/components/Layout/Nav/index.jsx
+++ src/components/Layout/Nav/index.jsx
@@ -1,8 +1,10 @@
 import { NavLink, useLocation } from 'react-router-dom';
 import LogoutButton from '../../Auth/LogoutButton';
+import { useToken } from '../../../stores/useUserStore';
 
 export default function Navbar() {
   const currentLocation = useLocation();
+  const token = useToken();
 
   return (
     <header>
@@ -10,31 +12,45 @@ export default function Navbar() {
         <div className='flex-1'>
           <NavLink
             to='/'
-            className={`btn btn-ghost text-xl ${
-              currentLocation.pathname === '/' ? 'font-bold underline' : ''
-            }`}
+            className={`btn btn-ghost text-xl ${currentLocation.pathname === '/' ? 'font-bold underline' : ''
+              }`}
           >
             Holidaze
           </NavLink>
         </div>
         <div className='flex-1'>
+          {!token ? (
+            <>
+              <NavLink
+                to='/login'
+                className={`btn btn-secondary text-xl ${currentLocation.pathname === '/login' ? 'font-bold underline' : ''
+                  }`}
+              >
+                Login
+              </NavLink>
+              <NavLink
+                to='/register'
+                className={`btn btn-ghost text-xl ${currentLocation.pathname === '/register' ? 'font-bold underline' : ''
+                  }`}
+              >
+                Register
+              </NavLink>
+            </>
+          ) : (
+            <>
           <NavLink
-            to='/login'
-            className={`btn btn-ghost text-xl ${
-              currentLocation.pathname === '/login' ? 'font-bold underline' : ''
-            }`}
-          >
-            Login
-          </NavLink>
-          <NavLink
-            to='/register'
-            className={`btn btn-ghost text-xl ${
-              currentLocation.pathname === '/register' ? 'font-bold underline' : ''
-            }`}
-          >
-            Register
-          </NavLink>
-          <LogoutButton />
+          to='/dashboard'
+          className={`btn btn-ghost text-xl ${
+            currentLocation.pathname === '/dashboard' ? 'font-bold underline' : ''
+          }`}
+        >
+          Dashboard
+        </NavLink>
+        <LogoutButton />
+        </>
+          )}
+        
+
         </div>
       </nav>
     </header>
