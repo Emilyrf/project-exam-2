80d8999f22a80a840d399b16d408843083f97ccc
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-02-02 17:33:36 +0100

    A registered user may update their avatar

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,5 +1,29 @@
-function DashboardPage() {
-    return <h1>Dashboard</h1>
-  }
-  
-  export default DashboardPage;
+import React from 'react';
+import { useUser } from '../../stores/useUserStore';
+import UpdateProfileForm from './UpdateAvatarForm';
+
+const DashboardPage = () => {
+  const user = useUser();
+  return (
+    <div className='max-w-screen-xl mx-auto p-4 mt-8 md:mt-12 lg:mt-16'>
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure><img src={user.avatar} alt="User Avatar" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">Welcome, {user ? user.name : 'Guest'}</h2>
+          <h2>Email: {user.email}</h2>
+          <button className="btn btn-primary" onClick={() => document.getElementById('my_modal_3').showModal()}>Edit avatar</button>
+          <dialog id="my_modal_3" className="modal">
+            <div className="modal-box">
+              <UpdateProfileForm/>
+            </div>
+          </dialog>
+        </div>
+      </div>
+      <h2>Upcoming bookings:</h2>
+      <h2>Your bookings:</h2>
+      <h2>Your venues:</h2>
+    </div>
+  );
+};
+
+export default DashboardPage;
--- src/stores/useUserStore.jsx
+++ src/stores/useUserStore.jsx
@@ -7,22 +7,30 @@ const useUserStore = create(
       user: null,
       isVenueManager: false,
       setUser: (user) => set({ user }),
-      setIsVenueManager: (value) => set({ isVenueManager: value }), 
+      setIsVenueManager: (value) => set({ isVenueManager: value }),
       clearUser: () => set({ user: null }),
+      setUserSubs: (userSubs) => set({ userSubs }),
     }),
     {
       name: 'user',
-    },
-  ),
+    }
+  )
 );
+export default useUserStore;
 
-export const useToken = () => useUserStore((state) => state.user?.accessToken);
-export const useUser = () => useUserStore((state) => state.user);
-export const useIsVenueManager = () => useUserStore((state) => state.isVenueManager);
+export const useToken = () => useUserStore.getState().user?.accessToken;
+export const useUser = () => useUserStore.getState().user;
+export const useIsVenueManager = () => useUserStore.getState().isVenueManager;
 
 export const useUserActions = () => {
-  const { setUser,setIsVenueManager, clearUser } = useUserStore();
-  return { setUser, setIsVenueManager,  clearUser };
+  const { setState } = useUserStore();
+
+  // Use utility functions for setting state
+  const setUser = (user) => setState({ user });
+  const setIsVenueManager = (value) => setState({ isVenueManager: value });
+  const clearUser = () => setState({ user: null });
+
+  return { setUser, setIsVenueManager, clearUser };
 };
 
 export const useUserName = () => {
