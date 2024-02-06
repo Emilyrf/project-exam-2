21483ea59853ba8fe4e73d40713c65b6a77b7814
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2024-02-06 12:09:35 +0100

    Adds location selectors

---
+++ src/components/LocationSelect/ContinentSelect.jsx
@@ -0,0 +1,27 @@
+import Select from 'react-select';
+
+const continents = [
+  { value: 'AF', label: 'Africa' },
+  { value: 'AN', label: 'Antarctica' },
+  { value: 'AS', label: 'Asia' },
+  { value: 'EU', label: 'Europe' },
+  { value: 'NA', label: 'North America' },
+  { value: 'OC', label: 'Oceania' },
+  { value: 'SA', label: 'South America' },
+];
+
+const ContinentSelect = ({ name, label, register }) => {
+  return (
+    <div className='form-control'>
+      <label className='label' htmlFor={name}>
+        {label}:
+      </label>
+      <Select
+        options={continents}
+        {...register(name)}
+      />
+    </div>
+  );
+};
+
+export default ContinentSelect;
---
+++ src/components/LocationSelect/CountrySelect.jsx
@@ -0,0 +1,24 @@
+import Select from 'react-select';
+import CountryList from 'react-select-country-list';
+
+const countries = CountryList().getData();
+const countryOptions = countries.map(country => ({
+  value: country.value,
+  label: country.label
+}));
+
+const CountrySelect = ({ name, label, register }) => {
+  return (
+    <div className='form-control'>
+      <label className='label' htmlFor={name}>
+        {label}:
+      </label>
+      <Select
+        options={countryOptions}
+        {...register(name)}
+      />
+    </div>
+  );
+};
+
+export default CountrySelect;
